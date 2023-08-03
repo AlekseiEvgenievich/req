8e8ff9fe6ecabbf612f68cf9b3a9961d333e860d
AlekseiEvgenievich <aleksei.smirnov@phystech.edu>
2023-08-03 15:13:34 +0200

    Update req.py

--- req.py
+++ req.py
@@ -1,10 +1,11 @@
 import requests
 
 names_cities = ("Лондон", "Шереметьево", "Череповец")
+payload = {"nTqm": "", "lang": "ru"}
 
 for city in names_cities:
-    url_temp = "https://wttr.in/{}?nTqm&lang=ru"
+    url_temp = "https://wttr.in/{}"
     url = url_temp.format(city)
-    response = requests.get(url)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
     print(response.text)
